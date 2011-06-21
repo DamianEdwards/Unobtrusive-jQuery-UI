8c92a0f61cc19599f2832b1b4b1117eb34a2bf1e
DamianEdwards <damian@damianedwards.com>
2011-06-21 10:56:23 -0700

    library updated

--- src/jquery-ui.unobtrusive.js
+++ src/jquery-ui.unobtrusive.js
@@ -5,37 +5,36 @@
  * http://www.opensource.org/licenses/MS-PL
  */
 (function ($) {
-
+	"use strict";
+	
     $(function () {
-
+        var prefix = "data-ui-";
+        
         // Wire-up jQuery UI unobtrusively
-        $("*[data-ui-fn]").each(function () {
+        $("*[" + prefix + "fn]").each(function () {
             var el = this,
-                $el = $(this);
+                $el = $(el);
 
             // Loop through functions in data-ui-fn attribute
-            $.each($el.attr("data-ui-fn").split(" "), function () {
+            $.each($el.attr(prefix + "fn").split(" "), function () {
                 var fn = this,
                     options = {},
-                    optionPrefix = "";
-
-                optionPrefix = "data-ui-" + fn + "-";
+                    optionPrefix = prefix + fn + "-";
 
                 // Build options parameter from data-ui-[fn]-* attributes
                 $.each(el.attributes, function () {
                     var attr = this;
-					if (!attr.specified) return true;
-					
-                    if (attr.name.indexOf(optionPrefix) === 0) {
-						options[attr.name.substr(optionPrefix.length)] = attr.value;
+                    if (!attr.specified) return true;
                     
+                    if (attr.name.indexOf(optionPrefix) === 0) {
+                        options[attr.name.substr(optionPrefix.length)] = attr.value;
+                    }
                 });
 
                 // Call jQuery UI fn if it exists
                 ($el[fn] || $.noop).call($el, options);
-
             });
         });
     });
 
-} (jQuery));
+}(window.jQuery));
