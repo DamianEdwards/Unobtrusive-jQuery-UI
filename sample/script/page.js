1858c39c96e831e05b6b51427130fd6732b9ee2f
DamianEdwards <damian@damianedwards.com>
2011-06-21 10:55:43 -0700

    library & sample added

---
+++ sample/script/page.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="jquery-1.6.1.js" />
+/// <reference path="jquery-ui.1.8.14.js" />
+$(function() {
+    
+    // Wire-up code samples
+    $("p.code-sample").before("<a class=\"show-link\" href=\"#\" title=\"Show the code example\">show example</a>");
+    
+    $("a.show-link").click(function(e) {
+        e.preventDefault();
+        var el = $(this);
+        el.next().toggle("fast", function() {
+            el.text(el.text() == "show example" ? "hide example" : "show example")
+        });
+        return false;
+    });
+
+});
---
+++ src/jquery-ui.unobtrusive.js
@@ -0,0 +1,41 @@
+﻿/*!
+ * Unobtrusive jQuery UI 0.1
+ * Copyright 2011, Damian Edwards http://damianedwards.com
+ * Licensed under Ms-PL
+ * http://www.opensource.org/licenses/MS-PL
+ */
+(function ($) {
+
+    $(function () {
+
+        // Wire-up jQuery UI unobtrusively
+        $("*[data-ui-fn]").each(function () {
+            var el = this,
+                $el = $(this);
+
+            // Loop through functions in data-ui-fn attribute
+            $.each($el.attr("data-ui-fn").split(" "), function () {
+                var fn = this,
+                    options = {},
+                    optionPrefix = "";
+
+                optionPrefix = "data-ui-" + fn + "-";
+
+                // Build options parameter from data-ui-[fn]-* attributes
+                $.each(el.attributes, function () {
+                    var attr = this;
+					if (!attr.specified) return true;
+					
+                    if (attr.name.indexOf(optionPrefix) === 0) {
+						options[attr.name.substr(optionPrefix.length)] = attr.value;
+                    
+                });
+
+                // Call jQuery UI fn if it exists
+                ($el[fn] || $.noop).call($el, options);
+
+            });
+        });
+    });
+
+} (jQuery));
