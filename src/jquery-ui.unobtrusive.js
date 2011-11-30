939d9d352d83fd0220a25e3b4324a3f9ee1f9d4e
omegaluz <omegaluz@gmail.com>
2011-11-30 13:00:33 -0600

    Update src/jquery-ui.unobtrusive.js

--- src/jquery-ui.unobtrusive.js
+++ src/jquery-ui.unobtrusive.js
@@ -1,19 +1,31 @@
-﻿/*!
- * Unobtrusive jQuery UI 0.1
- * Copyright 2011, Damian Edwards http://damianedwards.com
- * Licensed under Ms-PL
- * http://www.opensource.org/licenses/MS-PL
- */
+/*!
+* Unobtrusive jQuery UI 0.1
+* Copyright 2011, Damian Edwards http://damianedwards.com
+* Licensed under Ms-PL
+* http://www.opensource.org/licenses/MS-PL
+*/
+
+/*!
+* Modified by Andrew Cohen, 11/30/2011
+* TempWorks Software, Inc.
+*/
+
 (function ($) {
-	"use strict";
-	
-    $(function () {
-        var prefix = "data-ui-";
-        
-        // Wire-up jQuery UI unobtrusively
-        $("*[" + prefix + "fn]").each(function () {
-            var el = this,
-                $el = $(el);
+    "use strict";
+
+    var $jQui = $.ui,
+        prefix = "data-ui-";
+
+    $jQui.unobtrusive = {
+        parse: function (element) {
+            // Wire-up jQuery UI unobtrusively
+            $("*[" + prefix + "fn]", element).each(function () {
+                $jQui.unobtrusive.parseElement(this);
+            });
+        },
+        parseElement: function (el) {
+            var $el = $(el),
+                el = $el[0];
 
             // Loop through functions in data-ui-fn attribute
             $.each($el.attr(prefix + "fn").split(" "), function () {
@@ -25,16 +37,22 @@
                 $.each(el.attributes, function () {
                     var attr = this;
                     if (!attr.specified) return true;
-                    
+
                     if (attr.name.indexOf(optionPrefix) === 0) {
-                        options[attr.name.substr(optionPrefix.length)] = attr.value;
+                        // camelCase the name
+                        var attrName = $.camelCase(attr.name.substr(optionPrefix.length));
+                        options[attrName] = attr.value;
                     }
                 });
 
                 // Call jQuery UI fn if it exists
                 ($el[fn] || $.noop).call($el, options);
             });
-        });
+        }
+    }
+
+    $(function () {
+        $jQui.unobtrusive.parse(document);
     });
 
-}(window.jQuery));
+} (window.jQuery));
