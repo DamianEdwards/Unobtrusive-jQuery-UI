53e08c213224f74a4a1af18ad4942b75f995a906
DamianEdwards <damian@damianedwards.com>
2011-12-21 12:55:05 -0800

    Added more vsdoc comments. Renamed some variables. Fixed JSLint errors. Added more samples to demonstrate new features.

--- sample/script/page.js
+++ sample/script/page.js
@@ -1,17 +1,31 @@
-﻿/// <reference path="jquery-1.6.1.js" />
-/// <reference path="jquery-ui.1.8.14.js" />
-$(function() {
-    
+﻿/// <reference path="jquery-1.7.1.js" />
+/// <reference path="jquery-ui.1.8.17.js" />
+/// <reference path="../../src/jquery-ui.unobtrusive.js" />
+
+$(function () {
+
     // Wire-up code samples
     $("p.code-sample").before("<a class=\"show-link\" href=\"#\" title=\"Show the code example\">show example</a>");
-    
-    $("a.show-link").click(function(e) {
+
+    $("a.show-link").click(function (e) {
         e.preventDefault();
         var el = $(this);
-        el.next().toggle("fast", function() {
+        el.next().toggle("fast", function () {
             el.text(el.text() == "show example" ? "hide example" : "show example")
         });
         return false;
     });
 
+    // Create dynamic UI
+    $("#dynamic button").click(function (e) {
+        var html = "<label for='dateOfBirth'>Birth Date:</label>" +
+               "<input id='dateOfBirth' name='dateOfBirth' data-ui-fn='datepicker' />" +
+               "<div data-ui-fn='slider'></div>";
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        $("#dynamic .content").append(html);
+        $.ui.unobtrusive.parse("#dynamic .content");
+    });
 });
--- src/jquery-ui.unobtrusive.js
+++ src/jquery-ui.unobtrusive.js
@@ -1,5 +1,5 @@
-/*!
-* Unobtrusive jQuery UI 0.1
+﻿/*!
+* Unobtrusive jQuery UI 0.2
 * Copyright 2011, Damian Edwards http://damianedwards.com
 * Licensed under Ms-PL
 * http://www.opensource.org/licenses/MS-PL
@@ -17,65 +17,85 @@
         prefix = "data-ui-";
 
     function getFunction(fn) {
+        /// <summary>Takes a string and converts it to an anonymous function</summary>
         /// <param name="fn" type="String">String of function to parse</param>
         /// <returns type="Function" />
-        /// <summary>Takes a string and converts it to an anonymous function</summary>
-        var fnMatches = fn.match(/function\((.*?)\).*?\{(.*)\}$/);
+
+        var fnMatches = fn.match(/function\s*\((.*?)\).*?\{(.*)\}$/),
+            argsMatches,
+            argsString,
+            i;
+
         if (fnMatches) {
-            var argsMatches = fnMatches[1].split(','),
-                argsString = '';
-            for (var i = 0; i < argsMatches.length; i++) {
+            argsMatches = fnMatches[1].split(',');
+            argsString = '';
+            for (i = 0; i < argsMatches.length; i += 1) {
                 argsMatches[i] = '"' + $.trim(argsMatches[i]) + '"';
             }
             return eval('new Function(' + argsMatches.join(',') + ', "' + fnMatches[2] + '")');
         }
-    };
+    }
 
     $jQui.unobtrusive = {
-        parse: function (element) {
-            // Wire-up jQuery UI unobtrusively
-            $("*[" + prefix + "fn]", element).each(function () {
+
+        parse: function (context) {
+            /// <summary>Wires up jQuery UI unobtrusively based on data-* attributes for the passed element and its children.</summary>
+            /// <param name="context">A DOM Element, Document, or jQuery to use as context</param>
+
+            $("*[" + prefix + "fn]", context).each(function () {
                 $jQui.unobtrusive.parseElement(this);
             });
         },
+
         parseElement: function (el) {
+            /// <summary>Wires up jQuery UI unobtrusively on a single element.</summary>
+            /// <param name="el">A DOM Element, selector or jQuery collection.</param>
+
             var $el = $(el),
-                el = $el[0];
+                element = $el[0];
 
             // Loop through functions in data-ui-fn attribute
             $.each($el.attr(prefix + "fn").split(" "), function () {
                 var fn = this,
+                    uiFn,
                     options = {},
                     optionPrefix = prefix + fn + "-";
 
                 // Build options parameter from data-ui-[fn]-* attributes
-                $.each(el.attributes, function () {
-                    var attr = this;
-                    if (!attr.specified) return true;
+                $.each(element.attributes, function () {
+                    var attr = this,
+                        attrName;
+
+                    if (!attr.specified) {
+                        return true;
+                    }
 
                     if (attr.name.indexOf(optionPrefix) === 0) {
                         // camelCase the name
-                        if (attr.value.substr(0, 8) === 'function') { // test for anonymous function
+                        attrName = $.camelCase(attr.name.substr(optionPrefix.length));
+
+                        // test for anonymous function
+                        if (attr.value.substr(0, 8) === 'function') {
                             options[attrName] = getFunction(attr.value);
                         } else {
                             options[attrName] = attr.value;
                         }
-                        options[attrName] = attr.value;
                     }
                 });
 
                 // get UI fn if it exists
-                var uiFn = ($el[fn] || $.noop);
+                uiFn = ($el[fn] || $.noop);
                 // call destroy to remove the ui widget
                 uiFn.call($el, 'destroy');
                 // call fn with options
                 uiFn.call($el, options);
             });
         }
-    }
+    };
 
+    // onload, parse the whole page
     $(function () {
-        $jQui.unobtrusive.parse(document);
+        $jQui.unobtrusive.parse(window.document);
     });
 
-} (window.jQuery));
+}(window.jQuery));
